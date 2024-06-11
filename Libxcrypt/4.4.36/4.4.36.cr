c9720c2a3e52370672849b7297683be547bb15db
Fulgurance <zohran.londais@gmail.com>
2024-06-11 22:19:18 +0100

    Updated accessor name

--- Libxcrypt/4.4.36/4.4.36.cr
+++ Libxcrypt/4.4.36/4.4.36.cr
@@ -26,7 +26,7 @@ class Target < ISM::Software
                             path: buildDirectoryPath(entry: "MainBuild"))
 
         if option("32Bits")
-            configureSource([   "--host=i686-#{Ism.settings.targetName}-linux-gnu",
+            configureSource([   "--host=i686-#{Ism.settings.systemTargetName}-linux-gnu",
                                 "--prefix=/usr",
                                 "--libdir=/usr/lib32",
                                 "--enable-hashes=strong,glibc",
@@ -38,7 +38,7 @@ class Target < ISM::Software
         end
 
         if option("x32Bits")
-            configureSource([   "--host=#{Ism.settings.target}x32",
+            configureSource([   "--host=#{Ism.settings.systemTarget}x32",
                                 "--prefix=/usr",
                                 "--libdir=/usr/libx32",
                                 "--enable-hashes=strong,glibc",
