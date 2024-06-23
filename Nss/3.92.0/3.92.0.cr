bd3a7c060f3ded5651d1ec7162f26853bdb60e08
Fulgurance <zohran.londais@gmail.com>
2024-06-23 14:55:54 +0100

    Updated wrong path

--- Nss/3.92.0/3.92.0.cr
+++ Nss/3.92.0/3.92.0.cr
@@ -89,13 +89,13 @@ class Target < ISM::Software
 
         Dir["#{workDirectoryPathNoChroot}/dist/Linux*/lib/*.so"].each do |path|
             filename = path.lchop(path[0..path.rindex("/")])
-            destination = "/#{filename}"
+            destination = "/usr/lib/#{filename}"
             runChmodCommand("0755 #{destination}")
         end
 
         Dir["#{workDirectoryPathNoChroot}/dist/Linux*/lib/*.chk"].each do |path|
             filename = path.lchop(path[0..path.rindex("/")])
-            destination = "/#{filename}"
+            destination = "/usr/lib/#{filename}"
             runChmodCommand("0644 #{destination}")
         end
 
