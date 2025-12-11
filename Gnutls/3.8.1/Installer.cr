e4c0113be6c5fd0b28465929ca1b1c425ce84598
Fulgurance <zohran.londais@gmail.com>
2025-12-11 12:28:25 +0000

    Added missing versions

---
+++ Gnutls/3.8.1/Installer.cr
@@ -0,0 +1,26 @@
+class Target < ISM::Software
+
+    def configure
+        super
+
+        configureSource(arguments:  "--prefix=/usr                                                  \
+                                    --docdir=/usr/share/doc/#{versionName}                          \
+                                    #{option("Libunistring") ? "" : "--with-included-unistring"}    \
+                                    --with-default-trust-store-pkcs11=\"pkcs11:\"",
+                        path:       buildDirectoryPath)
+    end
+
+    def build
+        super
+
+        makeSource(path: buildDirectoryPath)
+    end
+    
+    def prepareInstallation
+        super
+
+        makeSource( arguments:  "DESTDIR=#{builtSoftwareDirectoryPath}#{Ism.settings.rootPath} install",
+                    path:       buildDirectoryPath)
+    end
+
+end
